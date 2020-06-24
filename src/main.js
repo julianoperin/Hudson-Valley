359c76a8875a89e467235af77d4bd45ff5d7ac9e
‘Juliano <julianoperins@gmail.com>
2020-06-24 02:28:38 -0400

    fixed the scrollMagic bug

--- src/main.js
+++ src/main.js
@@ -1,9 +1,39 @@
 import "./scss/app.scss";
 
-// import App from './App'
+let controller;
+let slideScene;
 
-// const app = async () => {
-//   document.getElementById('app').appendChild(await App())
-// }
-// // Load app
-// app()
+function animateSlides() {
+  //Init Controller
+  controller = new ScrollMagic.Controller();
+  //Select some things
+  const sliders = document.querySelectorAll(".slide");
+  const nav = document.querySelector(".nav-header");
+  //Loop over each sllide
+  sliders.forEach((slide, index, slides) => {
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
+    //GSAP
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+    slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
+    //Create Scene
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.4,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      .addIndicators({
+        colorStart: "black",
+        colorTrigger: "blue",
+        name: "slide",
+      })
+      .addTo(controller);
+  });
+}
+animateSlides();
